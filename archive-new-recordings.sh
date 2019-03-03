0cc84dc729f02b811fad2a037cd41bedd280bd27
Kevin Van Leer <kevin.vanleer@gmail.com>
2019-03-03 09:59:55 -0600

    Updated script output

--- archive-new-recordings.sh
+++ archive-new-recordings.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 REPO=/Users/kvl/Multimedia
 SRC_DIR=/Users/kvl/src/tablo_tools
@@ -7,6 +7,10 @@ pkill ffmpeg
 mkdir ${REPO}
 echo "Mounting ${REPO}"
 mount -t smbfs //media_archiver:y9i2ejnGk3U1ei8WZa@nas01/Multimedia ${REPO}
+if [ $? != 0 ] ; then
+    echo "Could not mount ${REPO}, exiting..."
+    exit 1
+fi
 caffeinate -s ${SRC_DIR}/venv3/bin/python ${SRC_DIR}/tablo_util.py record new --repo=${REPO}/TvShows
 echo "Unmounting ${REPO}"
 umount ${REPO}
